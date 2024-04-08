ee94a97f2204af8b05e41bc040d0dc1688c3069e
Ashish <ashishrajle700@gmail.com>
2024-04-08 14:47:13 +0530

    project init

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { Button } from "flowbite-react";
+export default function App() {
+    return (
+        <div>
+            <h1 className="text-red-500 text-3xl">Dashboard</h1>
+            <Button>Button</Button>
+        </div>
+    );
+}
